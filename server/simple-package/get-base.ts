8f3df4fa30dd7b580454ba8c5581c5bc3df8136a
GongT <admin@gongt.me>
2017-06-29 19:52:41 +0800

    fix name error

--- server/simple-package/get-base.ts
+++ server/simple-package/get-base.ts
@@ -2,7 +2,7 @@ import {JsonEnv} from "@gongt/jenv-data";
 import {PROJECT_NAME} from "./global";
 
 export function getBaseUrl() {
-	let base = (JsonEnv && JsonEnv.services["jspm-library"].serverUrl) ||
+	let base = (JsonEnv && JsonEnv.services.jspm.serverUrl) ||
 	           '//' + PROJECT_NAME + '.' + JsonEnv.baseDomainName + '/';
 	if (!/\/$/.test(base)) {
 		base += '/';
