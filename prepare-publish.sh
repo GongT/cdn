d22f0c394f7e5682ad3583d79366207192539e56
GongT <admin@gongt.me>
2017-07-04 18:12:47 +0800

    force version

--- prepare-publish.sh
+++ prepare-publish.sh
@@ -7,4 +7,7 @@ tsc -p server
 tsc -p client
 tsc -p server/simple-package
 
+cd server/simple-package/
+ncu -a -u
+cd ../..
 cp -f server/simple-package/package.json dist/simple-package/package.json
